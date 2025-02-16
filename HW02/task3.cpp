db77a619dac0f747d121d8c90f7b836a420288c9
Anchoret13 <vergil1133@live.com>
2025-02-16 10:22:00 -0600

    HW02

--- HW02/task3.cpp
+++ HW02/task3.cpp
@@ -15,14 +15,12 @@ void printResults(double time_ms, const double* C, unsigned int n, const std::st
 
 int main(int argc, char* argv[])
 {
-    // 1) Parse command-line arguments (if your assignment requires it).
     if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " n\n";
         return EXIT_FAILURE;
     }
     unsigned int n = std::stoul(argv[1]);
 
-    // 2) Generate random matrices A and B of size n x n in row-major order.
     std::vector<double> A(n * n), B(n * n);
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -33,10 +31,9 @@ int main(int argc, char* argv[])
         B[i] = dist(gen);
     }
 
-    // 3) Allocate memory for the result matrices.
     std::vector<double> C1(n * n), C2(n * n), C3(n * n), C4(n * n);
 
-    // 4) mmul1
+    // mmul1
     {
         auto start = std::chrono::high_resolution_clock::now();
         mmul1(A.data(), B.data(), C1.data(), n);
@@ -45,7 +42,7 @@ int main(int argc, char* argv[])
         printResults(time_ms, C1.data(), n, "mmul1");
     }
 
-    // 5) mmul2
+    // mmul2
     {
         auto start = std::chrono::high_resolution_clock::now();
         mmul2(A.data(), B.data(), C2.data(), n);
@@ -54,7 +51,7 @@ int main(int argc, char* argv[])
         printResults(time_ms, C2.data(), n, "mmul2");
     }
 
-    // 6) mmul3
+    // mmul3
     {
         auto start = std::chrono::high_resolution_clock::now();
         mmul3(A.data(), B.data(), C3.data(), n);
@@ -63,9 +60,7 @@ int main(int argc, char* argv[])
         printResults(time_ms, C3.data(), n, "mmul3");
     }
 
-    // 7) mmul4
-    //    Notice that mmul4 expects A and B as std::vector<double>&,
-    //    so we can pass A and B directly, but we pass C4 as a raw pointer.
+    // mmul4
     {
         auto start = std::chrono::high_resolution_clock::now();
         mmul4(A, B, C4.data(), n);
